a789fceee16831cd2484fa8771c2f8a5a1f47322
Gustavo <gramirez59@misena.edu.co>
2019-11-06 11:41:49 -0500

    Actualizando fuentes

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { InicioSesionComponent } from './inicio-sesion/inicio-sesion.component';
+import { ListaProductosComponent } from './lista-productos/lista-productos.component';
+import { RegistroComponent } from './registro/registro.component';
+import { DetalleProductoComponent } from './detalle-producto/detalle-producto.component';
+import { CarritoComprasComponent } from './carrito-compras/carrito-compras.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatGridListModule } from '@angular/material/grid-list';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    InicioSesionComponent,
+    ListaProductosComponent,
+    RegistroComponent,
+    DetalleProductoComponent,
+    CarritoComprasComponent
+  ],
+  imports: [
+    BrowserModule,
+    NoopAnimationsModule,
+    MatGridListModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
